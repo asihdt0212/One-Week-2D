2c8a80c6ce3f8b6a61af63aeae59f14be0187cdb
jinhuyukawa <tanabata2020y@gmail.com>
2020-08-10 01:48:36 +0900

    タイトルシーン仮　実装

---
+++ Assets/Scripts/Defines/HierarchyPath_Game.cs
@@ -0,0 +1,21 @@
+﻿public static class HierarchyPath_Game
+{
+	public struct MainCamera
+	{
+		public const string Root = "Main Camera";
+	}
+	public struct GameManager
+	{
+		public const string Root = "GameManager";
+	}
+	public struct TitleUICanvas
+	{
+		public const string Root = "TitleUICanvas";
+		public const string StartButton = "TitleUICanvas/StartButton";
+		public const string StartButtonStartButtonLabel = "TitleUICanvas/StartButton/StartButtonLabel";
+	}
+	public struct EventSystem
+	{
+		public const string Root = "EventSystem";
+	}
+}
---
+++ Assets/Scripts/Defines/HierarchyPath_Title.cs
@@ -0,0 +1,20 @@
+﻿public static class HierarchyPath_Title
+{
+	public struct MainCamera
+	{
+		public const string Root = "Main Camera";
+	}
+	public struct TitleUICanvas
+	{
+		public const string Root = "TitleUICanvas";
+		public const string TitleLabel = "TitleUICanvas/TitleLabel";
+		public const string StartButton = "TitleUICanvas/StartButton";
+		public const string StartButtonStartButtonLabel = "TitleUICanvas/StartButton/StartButtonLabel";
+		public const string LoadingPanel = "TitleUICanvas/LoadingPanel";
+		public const string LoadingPanelText = "TitleUICanvas/LoadingPanel/Text";
+	}
+	public struct EventSystem
+	{
+		public const string Root = "EventSystem";
+	}
+}
