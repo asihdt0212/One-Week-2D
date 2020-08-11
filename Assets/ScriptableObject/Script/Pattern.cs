e55a81243e2d3c7c3d96d47bc8c7dfdb85e17196
asihdt <asihdt@outlook.jp>
2020-08-12 04:07:03 +0900

    入っていくキャラの自動生成を追加。

--- Assets/ScriptableObject/Script/Pattern.cs
+++ Assets/ScriptableObject/Script/Pattern.cs
@@ -41,8 +41,10 @@ public abstract class Pattern : ScriptableObject
 
 
     //グループ人数
-    [Range(1, 5)]
-    public int People = 1;
+    [Range(1, 1)]
+    public int Human = 1;
+
+
 
     //出現から移動 false、待機から移動消える true
     public bool m_MoveFlag = false;
@@ -75,31 +77,39 @@ public static class PatternExtension
 }
 //単純な出入りのパターン
 [CreateAssetMenu(menuName = "Pattern/MovePattern")]
-public class MovePatternLeft : Pattern
+public class MovePattern : Pattern
 {
     //動かすために必要
-    public Transform Mytransform;
+    private Transform Mytransform;
     //最初の待機時間？
-    public float FirstWaitTime = 0.0f;
+    private float FirstWaitTime = 0.0f;
     //目指す移動位置
-    public GameObject TargetObject;
+    private GameObject TargetObject;
     //移動時間
-    public float MoveTime = 5.0f;
+    private float MoveTime = 5.0f;
     //移動出現方向
-    public Angle PatternType_ = Angle.Up;
-    Vector3 MoveAngle_ = Vector3.zero;
+    private Angle PatternType_ = Angle.Up;
+    private Vector3 MoveAngle_ = Vector3.zero;
     //ターゲットから離れている長さ
-    public float LenthValue = 8;
-    //移動前の初期化 //動かすオブジェクトの位置データ参照データを引数。
-    public void MoveAngleInit(Transform transform)
+    const float LenthValue = 4.0f;
+    //何番目か
+    private int MyNumber = 0;
+    public MovePattern(int Number,Transform Transform ,GameObject TargetObj)
     {
-        
-        //オブジェクトのTransform参照データの受け渡し。
-        Mytransform = transform;
-        //オブジェクト位置を変える
-        Mytransform.position = TargetObject.transform.position;
+        //自分の番号
+        MyNumber = Number;
 
-        Debug.Log(Mytransform.position);
+        //移動させるTransformデータ
+        Mytransform = Transform;
+
+        //家データ
+        TargetObject = TargetObj;
+    }
+
+    //移動前の初期化 //動かすオブジェクトの位置データ参照データを引数。
+    public void MoveAngleInit()
+    {
+        //移動ベクトル初期化
         MoveAngle_ = Vector3.zero;
 
         foreach (KeyValuePair<Angle, Vector3> M_Angle in MoveAngle)
@@ -120,20 +130,8 @@ public class MovePatternLeft : Pattern
             ////逆側の位置を求めに行く
             //逆方向は+-4した値
             int Value = ((int)PatternType_ + 4 > MoveAngle.Count - 1) ? (int)PatternType_ + 4 - (MoveAngle.Count) : (int)PatternType_ + 4;
-            /*
-            //Valueが超えた時は移動方向の種類分減らす
-            if(Value > MoveAngle.Count - 1)
-            {
 
-                Debug.Log(Value + ">"+  (MoveAngle.Count - 1));
-                //超えた分
-                Value = Value - (MoveAngle.Count);
-            }
-            */
-            //Debug.Log("PatternType_" + PatternType_ + " "+ Value);
-            //値の入れ物
             Vector3 FirstPosi = Vector3.zero;
-
 
             foreach (KeyValuePair<Angle, Vector3> M_Angle in MoveAngle)
             {
@@ -146,11 +144,9 @@ public class MovePatternLeft : Pattern
                 }
             }
           
-
-            Debug.Log(FirstPosi);
             //オブジェクトの位置を変える
             Mytransform.position = TargetObject.transform.position + (FirstPosi* LenthValue);
-            //Debug.Log(Mytransform.position);
+  
         }
         else
         {
@@ -159,35 +155,77 @@ public class MovePatternLeft : Pattern
         }
     }
     //入る方向Left設定
-    public void HomeInInit()
+    public void RondomHomeInInit()
     {
-        //Left  or Down
-        PatternType_ = Angle.Left;
+        
+        //乱数でLeftかUpを決める
+        float RandomValue = Random.Range(0f, 1f)*10;
 
+        //Left Upの選択
+        if(RandomValue > 5)
+        {
+            PatternType_ = Angle.Right;
+        }
+        else if(RandomValue < 5)
+        {
+            PatternType_ = Angle.Down;
+        }
+        else
+        {
+            //再帰でももう一で設定する。
+            RondomHomeInInit();
+            return;
+        }
+        //入る選択
         m_MoveFlag = false;
-
+        //アクティブ状態を移動に変更
         ActiveMove_ = ActiveMove.Move;
     }
     //出るほうの処理を行います。　引数移動方向
-    public void HomeOutInit(Home home_data)
+    public void RondomHomeOutInit()
     {
-        //Right or Up
-        if (!home_data.GainHuman(People))
+        //生成できるかチェック。
+        if (!CharactorManager.Instance.GetHome().GainHuman(Human))
         {
             Debug.Log("キャラクター生成できませんでした。");
-            ActiveMove_ = ActiveMove.Wait;
+            //生成できなかったので入るほうを作成
+            RondomHomeInInit();
             return;
         }
-        //Right or Up
-        PatternType_ = Angle.Right;
 
-        PatternType_ = Angle.Up;
+        //乱数でLeftかUpを決める
+        float RandomValue = Random.Range(0f, 1f) * 10;
 
+        //Left Upの選択
+        if (RandomValue > 5)
+        {
+            PatternType_ = Angle.Right;
+        }
+        else if (RandomValue < 5)
+        {
+            PatternType_ = Angle.Down;
+        }
+        else
+        {
+            //再帰でももう一で設定する。
+            RondomHomeOutInit();
+            return;
+        }
+ 
+        //出る方
         m_MoveFlag = true;
-
+        //アクティブ状態を移動に変更
         ActiveMove_ = ActiveMove.Move;
     }
-
+    //
+    public void HomeSelectInInit()
+    {
+        RondomHomeInInit();
+    }
+    public void HomeSelectOutInit()
+    {
+        RondomHomeOutInit();
+    }
     //Pattern１
     public override void Move()
     {
@@ -195,13 +233,15 @@ public class MovePatternLeft : Pattern
         if (!m_MoveFlag)
         {
             Mytransform.DOMove(new Vector3(TargetObject.transform.position.x, TargetObject.transform.position.y, Mytransform.position.z), MoveTime)
-                   .OnComplete(() => End());
+                .SetDelay(MyNumber)
+                .OnComplete(() => End());
         }
         //出る方の処理
         else
         {
             Mytransform.DOMove(new Vector3(Mytransform.position.x + MoveAngle_.x, Mytransform.position.y + MoveAngle_.y, Mytransform.position.z + MoveAngle_.z), MoveTime)
-                   .OnComplete(() => End());
+                .SetDelay(MyNumber)
+                .OnComplete(() => End());
         }
         Debug.Log("Move実行");
     }
@@ -220,313 +260,6 @@ public class MovePatternLeft : Pattern
     public void End()
     {
         if(ActiveMove_ == ActiveMove.Move)
-        {
-            //終了処理後
-            ActiveMove_ = ActiveMove.End;
-
-        }
-
-    }
-}
-//単純な出入りのパターン
-[CreateAssetMenu(menuName = "Pattern/MovePattern")]
-public class MovePatternRight : Pattern
-{
-    //動かすために必要
-    public Transform Mytransform;
-    //最初の待機時間？
-    public float FirstWaitTime = 0.0f;
-    //目指す移動位置
-    public GameObject TargetObject;
-    //移動時間
-    public float MoveTime = 5.0f;
-    //移動出現方向
-    public Angle PatternType_ = Angle.Up;
-    Vector3 MoveAngle_ = Vector3.zero;
-    //ターゲットから離れている長さ
-    public float LenthValue = 8;
-    //移動前の初期化 //動かすオブジェクトの位置データ参照データを引数。
-    public void MoveAngleInit(Transform transform)
-    {
-
-        //オブジェクトのTransform参照データの受け渡し。
-        Mytransform = transform;
-        //オブジェクト位置を変える
-        Mytransform.position = TargetObject.transform.position;
-
-        Debug.Log(Mytransform.position);
-        MoveAngle_ = Vector3.zero;
-
-        foreach (KeyValuePair<Angle, Vector3> M_Angle in MoveAngle)
-        {
-            //キーが同じデータ
-            if (PatternType_ == M_Angle.Key)
-            {
-                //移動ベクトルを入れる
-                MoveAngle_ = M_Angle.Value;
-                //Debug.Log(PatternType_ +" "+M_Angle.Value);
-            }
-
-        }
-
-        //初期位置設定
-        if (!m_MoveFlag)
-        {
-            ////逆側の位置を求めに行く
-            //逆方向は+-4した値
-            int Value = ((int)PatternType_ + 4 > MoveAngle.Count - 1) ? (int)PatternType_ + 4 - (MoveAngle.Count) : (int)PatternType_ + 4;
-            /*
-            //Valueが超えた時は移動方向の種類分減らす
-            if(Value > MoveAngle.Count - 1)
-            {
-
-                Debug.Log(Value + ">"+  (MoveAngle.Count - 1));
-                //超えた分
-                Value = Value - (MoveAngle.Count);
-            }
-            */
-            //Debug.Log("PatternType_" + PatternType_ + " "+ Value);
-            //値の入れ物
-            Vector3 FirstPosi = Vector3.zero;
-
-
-            foreach (KeyValuePair<Angle, Vector3> M_Angle in MoveAngle)
-            {
-                //同じキーがあるか検索
-                if ((int)M_Angle.Key == Value)
-                {
-                    //入れ物に、仮の場所データを入れる
-                    FirstPosi = M_Angle.Value;
-                    Debug.Log(M_Angle.Key + "発見" + FirstPosi);
-                }
-            }
-
-
-            Debug.Log(FirstPosi);
-            //オブジェクトの位置を変える
-            Mytransform.position = TargetObject.transform.position + (FirstPosi * LenthValue);
-            //Debug.Log(Mytransform.position);
-        }
-        else
-        {
-            //ターゲット（家）の位置を入れる。
-            Mytransform.position = TargetObject.transform.position;
-        }
-    }
-    //入る方向Left設定
-    public void HomeInInit()
-    {
-        //Left  or Down
-        PatternType_ = Angle.Right;
-
-        m_MoveFlag = false;
-
-        ActiveMove_ = ActiveMove.Move;
-    }
-    //出るほうの処理を行います。　引数移動方向
-    public void HomeOutInit(Home home_data)
-    {
-        //Right or Up
-        if (!home_data.GainHuman(People))
-        {
-            Debug.Log("キャラクター生成できませんでした。");
-            ActiveMove_ = ActiveMove.Wait;
-            return;
-        }
-        //Right or Up
-        PatternType_ = Angle.Left;
-
-        m_MoveFlag = true;
-
-        ActiveMove_ = ActiveMove.Move;
-    }
-
-    //Pattern１
-    public override void Move()
-    {
-        //入るほうの処理
-        if (!m_MoveFlag)
-        {
-            Mytransform.DOMove(new Vector3(TargetObject.transform.position.x, TargetObject.transform.position.y, Mytransform.position.z), MoveTime)
-                   .OnComplete(() => End());
-        }
-        //出る方の処理
-        else
-        {
-            Mytransform.DOMove(new Vector3(Mytransform.position.x + MoveAngle_.x, Mytransform.position.y + MoveAngle_.y, Mytransform.position.z + MoveAngle_.z), MoveTime)
-                   .OnComplete(() => End());
-        }
-        Debug.Log("Move実行");
-    }
-
-    //Pattern2 移動開始まで時間待つ
-    public override void Move2()
-    {
-
-        Mytransform.DOMove(new Vector3(Mytransform.position.x + MoveAngle_.x, Mytransform.position.y + MoveAngle_.y, Mytransform.position.z + MoveAngle_.z), MoveTime)
-            .SetDelay(1f)
-            .OnComplete(() => End());
-
-        Debug.Log("Move2実行");
-    }
-    //終了処理
-    public void End()
-    {
-        if (ActiveMove_ == ActiveMove.Move)
-        {
-            //終了処理後
-            ActiveMove_ = ActiveMove.End;
-
-        }
-
-    }
-}
-//単純な出入りのパターン
-[CreateAssetMenu(menuName = "Pattern/MovePattern")]
-public class MovePatternUp : Pattern
-{
-    //動かすために必要
-    public Transform Mytransform;
-    //最初の待機時間？
-    public float FirstWaitTime = 0.0f;
-    //目指す移動位置
-    public GameObject TargetObject;
-    //移動時間
-    public float MoveTime = 5.0f;
-    //移動出現方向
-    public Angle PatternType_ = Angle.Up;
-    Vector3 MoveAngle_ = Vector3.zero;
-    //ターゲットから離れている長さ
-    public float LenthValue = 8;
-    //移動前の初期化 //動かすオブジェクトの位置データ参照データを引数。
-    public void MoveAngleInit(Transform transform)
-    {
-
-        //オブジェクトのTransform参照データの受け渡し。
-        Mytransform = transform;
-        //オブジェクト位置を変える
-        Mytransform.position = TargetObject.transform.position;
-
-        Debug.Log(Mytransform.position);
-        MoveAngle_ = Vector3.zero;
-
-        foreach (KeyValuePair<Angle, Vector3> M_Angle in MoveAngle)
-        {
-            //キーが同じデータ
-            if (PatternType_ == M_Angle.Key)
-            {
-                //移動ベクトルを入れる
-                MoveAngle_ = M_Angle.Value;
-                //Debug.Log(PatternType_ +" "+M_Angle.Value);
-            }
-
-        }
-
-        //初期位置設定
-        if (!m_MoveFlag)
-        {
-            ////逆側の位置を求めに行く
-            //逆方向は+-4した値
-            int Value = ((int)PatternType_ + 4 > MoveAngle.Count - 1) ? (int)PatternType_ + 4 - (MoveAngle.Count) : (int)PatternType_ + 4;
-            /*
-            //Valueが超えた時は移動方向の種類分減らす
-            if(Value > MoveAngle.Count - 1)
-            {
-
-                Debug.Log(Value + ">"+  (MoveAngle.Count - 1));
-                //超えた分
-                Value = Value - (MoveAngle.Count);
-            }
-            */
-            //Debug.Log("PatternType_" + PatternType_ + " "+ Value);
-            //値の入れ物
-            Vector3 FirstPosi = Vector3.zero;
-
-
-            foreach (KeyValuePair<Angle, Vector3> M_Angle in MoveAngle)
-            {
-                //同じキーがあるか検索
-                if ((int)M_Angle.Key == Value)
-                {
-                    //入れ物に、仮の場所データを入れる
-                    FirstPosi = M_Angle.Value;
-                    Debug.Log(M_Angle.Key + "発見" + FirstPosi);
-                }
-            }
-
-
-            Debug.Log(FirstPosi);
-            //オブジェクトの位置を変える
-            Mytransform.position = TargetObject.transform.position + (FirstPosi * LenthValue);
-            //Debug.Log(Mytransform.position);
-        }
-        else
-        {
-            //ターゲット（家）の位置を入れる。
-            Mytransform.position = TargetObject.transform.position;
-        }
-    }
-    //入る方向Left設定
-    public void HomeInInit()
-    {
-     
-       
-        PatternType_ = Angle.Down;
-        //入るflagに
-        m_MoveFlag = false;
-
-        ActiveMove_ = ActiveMove.Move;
-    }
-    //出るほうの処理を行います。　引数移動方向
-    public void HomeOutInit(Home home_data)
-    {
-        //Right or Up
-        if (!home_data.GainHuman(People))
-        {
-            Debug.Log("キャラクター生成できませんでした。");
-            ActiveMove_ = ActiveMove.Wait;
-            return;
-        }
-
-        PatternType_ = Angle.Up;
-        //出るフラグに
-        m_MoveFlag = true;
-
-        ActiveMove_ = ActiveMove.Move;
-    }
-
-    //Pattern１
-    public override void Move()
-    {
-        //入るほうの処理
-        if (!m_MoveFlag)
-        {
-            Mytransform.DOMove(new Vector3(TargetObject.transform.position.x, TargetObject.transform.position.y, Mytransform.position.z), MoveTime)
-                   .OnComplete(() => End());
-        }
-        //出る方の処理
-        else
-        {
-            Mytransform.DOMove(new Vector3(Mytransform.position.x + MoveAngle_.x, Mytransform.position.y + MoveAngle_.y, Mytransform.position.z + MoveAngle_.z), MoveTime)
-                   .OnComplete(() => End());
-        }
-        Debug.Log("Move実行");
-    }
-
-    //Pattern2 移動開始まで時間待つ
-    public override void Move2()
-    {
-
-        Mytransform.DOMove(new Vector3(Mytransform.position.x + MoveAngle_.x, Mytransform.position.y + MoveAngle_.y, Mytransform.position.z + MoveAngle_.z), MoveTime)
-            .SetDelay(1f)
-            .OnComplete(() => End());
-
-        Debug.Log("Move2実行");
-    }
-    //終了処理
-    public void End()
-    {
-        if (ActiveMove_ == ActiveMove.Move)
         {
             //終了処理後
             ActiveMove_ = ActiveMove.End;
--- Assets/Scripts/Game/CharactorManager.cs
+++ Assets/Scripts/Game/CharactorManager.cs
@@ -7,19 +7,38 @@ public class CharactorManager : Singleton<CharactorManager>
 {
     [SerializeField]
     GameObject[] gameObjects;
-    public Pattern[] Patterns;
+    
     //Patternリスト
     List<Pattern> ListPattern = new List<Pattern>();
     //人間データの格納先
     List<Charactor> ListCharactor = new List<Charactor>();
     public GameObject TargetObj;
-
+    //家データ
     private Home Home_;
-
     
     List<GameObject> ListCharaObj = new List<GameObject>();
 
     protected SelectMode SelectMode_;
+
+    //Patternの作成パターン
+    /*
+    0　家にランダムで入る。
+    1　家からランダムで出る。出来なかったとき　0を実行
+    2　家に移動方向Rightで入る。
+    3　家に移動方向Downで入る。
+    4  家から移動方向Rightで出る。出来なかったとき　0を実行
+    5  家から移動方向Upで出る。出来なかったとき　0を実行
+    */
+    public enum CreatePatternType
+    {
+        RandomHomeInType,
+        RandomHomeOutType,
+        HomeInRightType,
+        HomeInDownType,
+        HomeOutRightType,
+        HomeOutUpType,
+    }
+    List<CreatePatternType> CreatePatternType_ = new List<CreatePatternType>();
 
 
     // Start is called before the first frame update
@@ -31,11 +50,54 @@ public class CharactorManager : Singleton<CharactorManager>
         ListPattern.Clear();
         ListCharactor.Clear();
         ListCharaObj.Clear();
+
+        CreatePatternType_.Clear();
+
+        //ランダムにCreatePatternType_に成分を入れる。
+        for(int i = 0;i < 9; i++)
+        {
+            CreatePatternType CreatePatternTypes;
+
+            float R_Value =  Random.Range(0f, 1f) * 59;
+
+            if (R_Value >= 0 && R_Value < 10)
+            {
+                CreatePatternTypes = CreatePatternType.RandomHomeInType;
+            }
+            else if (R_Value >= 10 && R_Value < 20)
+            {
+                CreatePatternTypes = CreatePatternType.RandomHomeOutType;
+            }
+            else if (R_Value >= 20 && R_Value < 30)
+            {
+                CreatePatternTypes = CreatePatternType.HomeInRightType;
+            }
+            else if (R_Value >= 30 && R_Value < 40)
+            {
+                CreatePatternTypes = CreatePatternType.HomeInDownType;
+            }
+            else if (R_Value >= 40 && R_Value < 50)
+            {
+                CreatePatternTypes = CreatePatternType.HomeOutRightType;
+            }
+            else if (R_Value >= 50 && R_Value < 60)
+            {
+                CreatePatternTypes = CreatePatternType.HomeOutUpType;
+            }
+            else
+            {
+                CreatePatternTypes = CreatePatternType.RandomHomeInType;
+            }
+
+            CreatePatternType_.Add(CreatePatternTypes);
+        }
+
+        
         //
 
         SelectMode_ = new SelectMode();
 
-        SelectMode_.MaxHumanValue = 1;
+        
 
         Home_ = new Home();
 
@@ -56,16 +118,50 @@ public class CharactorManager : Singleton<CharactorManager>
             ListCharactor.Add(CharactorObj.GetComponent<Charactor>());
 
             //Patternと動くオブジェクトを設定
-            var M_Pattern = new MovePatternLeft();
+            var M_Pattern = new MovePattern(i, CharactorObj.transform,TargetObj);
 
-            //移動方向の決定
-            M_Pattern.HomeInInit();
-            //団体人数
-            M_Pattern.People = 1;
-            //家データの設定
-            M_Pattern.TargetObject = TargetObj;
+            switch (CreatePatternType_[i])
+            {
+                case CreatePatternType.RandomHomeInType:
+
+                    //移動方向の決定
+                    M_Pattern.RondomHomeInInit();
+
+                    break;
+                case CreatePatternType.RandomHomeOutType:
+
+                    //移動方向の決定
+                    M_Pattern.RondomHomeOutInit();
+
+                    break;
+                case CreatePatternType.HomeInRightType:
+
+                    M_Pattern.HomeSelectInInit();
+
+                    break;
+                case CreatePatternType.HomeInDownType:
+
+                    M_Pattern.HomeSelectInInit();
+
+                    break;
+                case CreatePatternType.HomeOutRightType:
+
+                    M_Pattern.HomeSelectOutInit();
+
+                    break;
+                case CreatePatternType.HomeOutUpType:
+
+                    M_Pattern.HomeSelectOutInit();
+
+                    break;
+                default:
+                    break;
+            }
+            
+ 
+
             //移動方向の初期化
-            M_Pattern.MoveAngleInit(CharactorObj.transform);
+            M_Pattern.MoveAngleInit();
             //リストへ追加
             ListPattern.Add(M_Pattern);
             //親
@@ -83,7 +179,7 @@ public class CharactorManager : Singleton<CharactorManager>
         {
             //移動アップデート
             
-            if(ListPattern[i].GetActiveMove() == Pattern.ActiveMove.Move) ListPattern[i].Move();
+            ListPattern[i].Move();
 
             //動いているオブジェクトが、Endになった時
             if(ListPattern[i].GetActiveMove() == Pattern.ActiveMove.End)
@@ -93,14 +189,16 @@ public class CharactorManager : Singleton<CharactorManager>
                 {
                     Debug.Log("AddHuman");
                     //人間の加算処理。
-                    Home_.AddHuman(ListPattern[i].People);
+                    Home_.AddHuman(ListPattern[i].Human);
 
                     //待機状態に
                     ListPattern[i].SetAcitveMove(Pattern.ActiveMove.Wait);
 
                     ListCharaObj[i].gameObject.SetActive(false);
 
-                    HomeCanvasUI.instance.SetHumanText(Home_.GetHumanValue());
+                    //HomeCanvasUI.instance.SetHumanText(Home_.GetHumanValue());
+
+                    //全員の移動が完了時 GameUI.SetAnswerMode()を呼び出す。
                 }
             }
         }
